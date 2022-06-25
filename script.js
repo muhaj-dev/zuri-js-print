e10c6d0ae1e908209ecda139a43d9ad617c9f406
muhammod1 <muhammodholawale@gmail.com>
2022-06-25 18:56:11 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let nam = document.getElementById("name");
+
+let height = document.getElementById("height");
+
+let country = document.getElementById("country");
+
+
+let myName = "My name is Muhammod Ajibade";
+
+let myHeight = "I am 1.83m tall"
+
+let myCountry= "I am from Nigeria"
+
+nam.textContent = myName;
+
+height.textContent = myHeight;
+
+country.textContent = myCountry;
